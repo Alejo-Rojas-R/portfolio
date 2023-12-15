d0fb79e1dcd86dd443a7061de57d14863a73a065
Alejo-Rojas-R <alejandrorojas1152@gmail.com>
2023-12-15 14:52:16 -0500

    run build

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+
+export const NavBar = () => {
+    const [scrolling, setScrolling] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 50) {
+            setScrolling(true);
+        } else {
+            setScrolling(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <nav className={`transition ease-in-out px-10 fixed top-0 w-full z-50 ${scrolling ? 'bg-black py-4' : 'py-4'}`}>
+            <div className='max-w-[1040px] flex justify-between m-auto'>
+                <h1 className='text-xl font-burtons text-white'>🜇</h1>
+                <ul className='flex items-center gap-6'>
+                    <li className='text-white font-semibold hover:scale-110'><a href='#about-me'>About</a></li>
+                    <li className='text-white font-semibold hover:scale-110'><a href='#projects'>Projects</a></li>
+                    <li className='text-white font-semibold hover:scale-110'><a href='#experience'>Experience</a></li>
+                    <li className='text-white font-semibold hover:scale-110'><a href='#skills'>Skills</a></li>
+                    <li className='text-white font-semibold hover:scale-110'><a href='#studies'>Studies</a></li>
+                    {/*<li className='text-white font-semibold hover:scale-110'><a href='#contact'>Contact</a></li>*/}
+                    <li className='text-white font-semibold hover:scale-110'><a href='https://github.com/Alejo-Rojas-R' target='_blank'>Github</a></li>
+                    <li className='text-white transition ease-in-out hover:scale-110'>
+                        <a className='bg-gradient-to-r from-red-500 to-red-700 text-white px-4 py-2 rounded-md' href='https://drive.google.com/file/d/1lMDqA3gHhUqUO3VMCxNDtgsQVmkSMMJz/view?usp=sharing' target='_blank'>Resume</a>
+                    </li>
+                    {/*<li><BsFillMoonStarsFill className='cursor-pointer text-2xl' /></li>*/}
+                </ul>
+            </div>
+        </nav>
+    )
+}
---
+++ src/components/Skills.jsx
@@ -0,0 +1,42 @@
+export const Skills = () => {
+    const skills = [
+        { icon: 'azure-plain', name: 'Azure' },
+        { icon: 'bootstrap-plain', name: 'Bootstrap' },
+        {/* icon: 'csharp-plain', name: 'C#' */ },
+        { icon: 'css3-plain', name: 'CSS3' },
+        {/*icon:'dot-net-plain', name:'.net'*/ },
+        {/* icon: 'dotnetcore-plain', name: '.NET Core'*/ },
+        {/* icon: 'eslint-original', name: 'esLint' */ },
+        {/* icon: 'figma-plain', name: 'Figma' */ },
+        { icon: 'git-plain', name: 'Git' },
+        { icon: 'github-original', name: 'GitHub' },
+        { icon: 'html5-plain', name: 'HTML5' },
+        {/* icon: 'intellij-plain', name: 'IntelliJ'*/ },
+        { icon: 'java-plain', name: 'Java' },
+        { icon: 'javascript-plain', name: 'Javascript' },
+        { icon: 'jest-plain', name: 'Jest' },
+        { icon: 'jquery-plain', name: 'jQuery' },
+        {/* icon: 'jira-plain', name: 'Jira' */ },
+        { icon: 'mysql-plain', name: 'MySQL' },
+        { icon: 'php-plain', name: 'PHP' },
+        {/*icon: 'phpstorm-plain', name: 'PHP Storm'*/ },
+        { icon: 'react-original', name: 'React' },
+        { icon: 'redux-original', name: 'Redux' },
+        { icon: 'spring-plain', name: 'Spring' },
+        { icon: 'tailwindcss-plain', name: 'Tailwind' },
+    ];
+
+    return (
+        <div className='flex flex-wrap w-full max-w-[1040px] gap-4 justify-center mt-5'>
+            {
+                skills.map((skill, index) => (
+                    skill.name &&
+                    <div key={index} className='flex flex-col justify-center items-center w-24 h-24 bg-white rounded transition ease-in-out hover:scale-105'>
+                        <i className={`devicon-${skill.icon} colored text-4xl`} />
+                        <div className='mt-2 text-black font-bold'>{skill.name}</div>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
